090253560b2eb3cb99025b56e3bb01f1376b9bef
Surya Naga Sesank M <67413218+nagasesank@users.noreply.github.com>
2022-04-19 11:53:42 +0530

    Updated with Family xmls

--- Oval_Product.py
+++ Oval_Product.py
@@ -23,8 +23,10 @@ while True:
    data = pd.DataFrame(df,columns=['TEST ID','Common Test','Family','Object ID','Object Command'])
    data = data.where(data['TEST ID'] == test_id)
    states_data = pd.DataFrame(df,columns=['State', 'States Regex'])
+   family_data=pd.DataFrame(df,columns=['Family'])
    data = data.reset_index()
    states_data=states_data.reset_index()
+   family_data = family_data.reset_index()
    data=data.fillna(0)
    states_data=states_data.fillna(0)
    state = {}
@@ -33,16 +35,32 @@ while True:
    for index, row in data.iterrows():
       if row['Common Test'] != 0:
          Common_Test = row['Common Test'][-5:-1]
+         fam=row['Family']
+         if fam == "iosxe":
+            tst['product'] = "iosxe"
+         elif fam == "ios":
+             tst['product'] = "ios"
+         elif fam == "asa":
+              tst['product'] = "asa"
+         elif fam == "junos":
+              tst['product'] = "junos"
+         elif fam == "nxos":
+              tst['product'] = "nxos"
+         else:
+            print("Object id is Miss Match")
+           
          for index_state, states in states_data.iterrows():
             if states['State'] != 0 and Common_Test != 0 :
                states_val = states['State'][-4:]
                if states_val == Common_Test:
                   '''test_id,Comment,state_id for tst_id in the tmp file to generate the test template'''
+                  tst["product"] = row['Family']
                   tst["test_id"] = test_id
                   tst["Comment"] = row['Common Test'].split("(")[0]
                   tst["state_id"] = states['State']
                   '''test_id,Comment,state_id for state_id in the tmp file to generate the state template'''
                   state["state_id"] = states['State']
+                  state["product"] = row['Family']
                   state["Comment"] = row['Common Test'].split("(")[0]
                   state["state_regex"] = states['States Regex'] 
                   build_id(tst)
---
+++ Readme.md
@@ -0,0 +1,21 @@
+---------------------------------------------------------
+
+				OVAl Product Automation Tool
+				Author:M.Surya Sesank
+			    Desg:Security Researcher
+---------------------------------------------------------------
+
+###### Tool Usage:
+
+ <code> python3 Oval_Product.py 
+ Enter The Sheet Name: 
+ Enter the Test Id: </code> 
+ ###### Note : 
+ ###### Based upon your test id the xmls will Generate.
+ 
+## Example 
+Sheet name:NX-OS
+Test ID:oval:org.tanium.cisco.nxos.cve:tst:11118002
+Test ID:0
+
+it will generate the Product.xml file
