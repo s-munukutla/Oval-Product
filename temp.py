db9e712fc3e0bf3e7367a28ea19e7b56cd2b5815
Surya Naga Sesank M <67413218+nagasesank@users.noreply.github.com>
2022-04-21 11:32:47 +0530

    Add files via upload

--- temp.py
+++ temp.py
@@ -8,8 +8,8 @@ state_id = '''\n<line_state xmlns ="http://oval.mitre.org/XMLSchema/oval-definit
     <config_line operation ="pattern match" >{data[state_regex]}</config_line >
 </line_state>'''
 
-tst_jun = '''<show_test xmlns = "http://oval.mitre.org/XMLSchema/oval-definitions-5#{data[product]}" id = "{data[test_id]}" check_existence = "at_least_one_exists" comment = "Check for {data[Comment]} " version = "1" check = "all" >
-      <object object_ref = ""oval:org.tanium.cisco.{data[product]}.cve:obj:{data[object_id]}""/>
+tst_jun = '''<show_test xmlns = "http://oval.mitre.org/XMLSchema/oval-definitions-5#{data[product]}" id = "{data[test_id]}" check_existence = "at_least_one_exists" comment = "Check for {data[Comment]}" version = "1" check = "all" >
+      <object object_ref = "oval:org.tanium.{data[product]}.cve:obj:{data[object_id]}"/>
       <state state_ref = "{data[state_id]}"/>
 </show_test>'''
 
